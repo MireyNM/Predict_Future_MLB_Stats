a3e0426aaaa7329ff30d9c0df9424e2b1d559245
Nathalie Ouellette <nathalieaouellette@gmail.com>
2023-01-23 12:39:28 -0500

    adding the website info and work was done in postman

---
+++ app.py
@@ -0,0 +1,22 @@
+import os 
+from cProfile import run
+import datetime as dt
+import numpy as np
+import pandas as pd
+from flask import Flask, jsonify, render_template, request
+
+# env | grep FLASK
+# export FLASK_APP=app.py
+# flask run 
+# http://127.0.0.1:5000/
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST'])
+def getResults():
+    data = request.json
+    print(data)
+    return ('Base was stolen')
