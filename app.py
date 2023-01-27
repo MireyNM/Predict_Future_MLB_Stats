7b996759da6ced0c59f145e86016952d4ebca527
Nathalie Ouellette <nathalieaouellette@gmail.com>
2023-01-26 20:19:10 -0500

    Adding nav bar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os 
+import pickle
 from cProfile import run
 import datetime as dt
 import numpy as np
@@ -10,14 +11,28 @@ from flask import Flask, jsonify, render_template, request
 # flask run 
 # http://127.0.0.1:5000/
 app = Flask(__name__)
+with open('Data/Clean_Data/next_war.js') as f:
+    model = f.readlines()
+    print("MODEL LOADED")
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route('/result', methods=['POST'])
 def getResults():
-    data = request.json
-    print(data)
-    print('test')
-    return ('Base was stolen')
+    # data = request.json
+    test_data = []
+    features = ['Name']
+    for feature in features:
+        test_data.append(request.form.get(feature))
+    print('Values received from the html form')
 
+    print(test_data)
+    prediction = model.predict(np.array(test_data).reshape(1,-1))
+    output = prediction[0]
+    return render_template("index.html", predicted_value=output)
+    # return ('test')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,12 @@
 // import the data from data.js
-const tableData = data;
+// const tableData = next_war;
+// print(tableData)
 
 // Reference the HTML table using d3
-var tbody = d3.select("tbody");
+// var tbody = d3.select("tbody");
+
+fetch('http://127.0.0.1:5000/',{
+    method: "POST"
+})
+    .then((response) => response.json())
+    .then((data) => console.log(data));
