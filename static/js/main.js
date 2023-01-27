cbf243498a86a53183c1fccb6cf98434dece49e7
Nathalie Ouellette <nathalieaouellette@gmail.com>
2023-01-26 20:19:37 -0500

    js acting weird

--- static/js/main.js
+++ static/js/main.js
@@ -5,8 +5,8 @@
 // Reference the HTML table using d3
 // var tbody = d3.select("tbody");
 
-fetch('http://127.0.0.1:5000/',{
-    method: "POST"
-})
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+// fetch('http://127.0.0.1:5000/',{
+//     method: "POST"
+// })
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
