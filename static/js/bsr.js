c262b52eb3f44988deb405377f5326d4537f661a
MireyNM <mir.nehme@gmail.com>
2023-01-31 17:39:16 -0500

    Autocomplete 1 name

--- static/js/bsr.js
+++ static/js/bsr.js
@@ -1,9 +1,11 @@
-var playerNames = tableData.map(player => player.Name);
+var playerNames = [...new Set(tableData.map(player => player.Name))];
 autocomplete(document.getElementById("batterBsRName"), playerNames);
 
 
 
 function getPredictedBsR() {
     var name = document.getElementById("batterBsRName").value;
+    var res = tableData.filter(player => player.Name == name).map(player => [player.Season, player.Next_WAR])
+    console.log(res);
     document.getElementById("BsRBATTResult").textContent = tableData.find(player => player.Name == name).Next_WAR;
 }
