d30b5dbe7d68d22e724e113b3f506f70cb808db0
Nathalie Ouellette <nathalieaouellette@gmail.com>
2023-02-07 16:20:15 -0500

    Correcting little mistakes

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-// import the data from next_BsR_batt.js
-// var tableData = data;
-// console.log(tableData)
-
-// import the data from next_war.js
-// const tableDataWar = data_war;
-// console.log(tableDataWar)
-
-
